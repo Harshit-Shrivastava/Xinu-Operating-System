b3506bd527033a7d8996cf636391f2d19fad96d3
vensamba <vensamba@indiana.edu>
2016-09-15 20:26:24 -0400

    Added the help option

--- shell/xsh_hello.c
+++ shell/xsh_hello.c
@@ -11,6 +11,19 @@
 shellcmd xsh_hello(int nargs, char *args[]) {
 
 	/* Check argument count */
+	if (nargs == 2 && strncmp(args[1], "--help", 7) == 0) {
+
+		printf("Use:\n");
+		printf("\t%s [command]\n", args[0]);
+		printf("Description:\n");
+		printf("\tProvides a list of shell commands or\n");
+		printf("\thelp information for a specific command\n");
+		printf("Options:\n");
+		printf("\tcommand\tspecific command for which to\n");
+		printf("\t\tdisplay help information\n");
+		printf("\t--help\tdisplay this help and exit\n");
+		return 0;
+	}
 
 	if (nargs > 2) {
 		fprintf(stderr, "%s: too many arguments\n", args[0]);
