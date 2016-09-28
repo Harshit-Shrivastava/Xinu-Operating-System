8d489f6084dbc25168f36fe1cbc81798d4806af4
hshrivas <hshrivas@indiana.edu>
2016-09-28 16:47:18 -0400

    Fixed the syncronization bug

--- shell/xsh_prodcons.c
+++ shell/xsh_prodcons.c
@@ -50,9 +50,10 @@ shellcmd xsh_prodcons(int32 nargs, char *args[])
 
 	//create the process producer and consumer and put them in ready queue.
 	//Look at the definations of function create and resume in the system folder for reference.      
-	consumed = semcreate(0);
-	produced = semcreate(1);
-	resume( create(producer, 1024, 20, "producer", 3, consumed, produced, count));
+	consumed = semcreate(1);
+	produced = semcreate(0);
+	resume( create(producer, 1024, 20, "consumer", 3, consumed, produced, count));
+	//resume( create(producer, 1024, 20, "producer", 3, consumed, produced, count));
 	resume( create(consumer, 1024, 20, "consumer", 3, consumed, produced, count));
 	return (0);
 }
