2536ce90fdee1e02de77536fbe60041b2b62b56b
vensamba <vensamba@indiana.edu>
2016-10-01 19:00:14 -0400

    added prodcons file

--- include/prodcons.h
+++ include/prodcons.h
@@ -1,9 +1,20 @@
-/*Global variable for producer consumer*/
-extern int32 n; /*this is just declaration*/
+#include <xinu.h>
+#include <stddef.h>
+#include <stdio.h>
+#include <future.h>
+#include <string.h>
 
-/*function Prototype*/
-void consumer(sid32, sid32, int32);
-void producer(sid32, sid32, int32);
+typedef unsigned int uint;
 
-/*Semaphone declarations*/
-extern sid32 produced, consumed;
+	future *f1, *f2, *f3;
+   /*Global variable for producer consumer*/
+   extern int32 n; /*this is just declaration*/
+
+ /* Declare the required semaphores */
+   extern sid32 consumed, produced; 
+  /*function Prototype*/
+ void consumer(sid32 consumed, sid32 produced, int32 count));
+ void producer(sid32 consumed, sid32 produced, int32 count);
+
+ uint future_cons(future *fut);
+ uint future_prod(future *fut);
