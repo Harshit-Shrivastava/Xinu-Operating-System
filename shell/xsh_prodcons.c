e758655460c21468cf7283633a18c18be231e4b1
vensamba <vensamba@indiana.edu>
2016-10-03 22:54:53 -0400

    code working as per requirement

--- shell/xsh_prodcons.c
+++ shell/xsh_prodcons.c
@@ -98,15 +98,15 @@ shellcmd xsh_prodcons(int32 nargs, char *args[])
 
 	else{
           f1 = future_alloc(FUTURE_EXCLUSIVE);
-	  //f2 = future_alloc(FUTURE_EXCLUSIVE);
-	  //f3 = future_alloc(FUTURE_EXCLUSIVE);
+	  f2 = future_alloc(FUTURE_EXCLUSIVE);
+	  f3 = future_alloc(FUTURE_EXCLUSIVE);
  
 	  resume( create(future_cons, 1024, 20, "fcons1", 1, f1) );
 	  resume( create(future_prod, 1024, 20, "fprod1", 1, f1) );
-	  //resume( create(future_cons, 1024, 20, "fcons2", 1, f2) );
-	  //resume( create(future_prod, 1024, 20, "fprod2", 1, f2) );
-	  //resume( create(future_cons, 1024, 20, "fcons3", 1, f3) );
-	  //resume( create(future_prod, 1024, 20, "fprod3", 1, f3) );	
+	  resume( create(future_cons, 1024, 20, "fcons2", 1, f2) );
+	  resume( create(future_prod, 1024, 20, "fprod2", 1, f2) );
+	  resume( create(future_cons, 1024, 20, "fcons3", 1, f3) );
+	  resume( create(future_prod, 1024, 20, "fprod3", 1, f3) );	
 	}
 
 return (0);
